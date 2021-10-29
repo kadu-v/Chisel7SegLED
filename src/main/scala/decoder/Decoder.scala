b66645c6e542eb28b91f72956c088871f9e6a967
kadu-v <kazuonsunday@yahoo.co.jp>
2021-10-29 18:54:24 +0900

    Fix: decoder

--- src/main/scala/decoder/Decoder.scala
+++ src/main/scala/decoder/Decoder.scala
@@ -14,16 +14,16 @@ class Decoder() extends Module {
   dout := MuxCase(
     0x77.asUInt(),
     Seq(
-      (io.din === 0x61.asUInt()) -> 0x7e.U(8.W),
-      (io.din === 0x62.asUInt()) -> 0x30.U(8.W),
-      (io.din === 0x63.asUInt()) -> 0x6d.U(8.W),
-      (io.din === 0x64.asUInt()) -> 0x79.U(8.W),
-      (io.din === 0x65.asUInt()) -> 0x33.U(8.W),
-      (io.din === 0x66.asUInt()) -> 0x5b.U(8.W),
-      (io.din === 0x67.asUInt()) -> 0x5f.U(8.W),
-      (io.din === 0x68.asUInt()) -> 0x70.U(8.W),
-      (io.din === 0x69.asUInt()) -> 0x7f.U(8.W),
-      (io.din === 0x7a.asUInt()) -> 0x7b.U(8.W)
+      (io.din === 0x30.asUInt()) -> 0x7e.U(8.W),
+      (io.din === 0x31.asUInt()) -> 0x30.U(8.W),
+      (io.din === 0x32.asUInt()) -> 0x6d.U(8.W),
+      (io.din === 0x33.asUInt()) -> 0x79.U(8.W),
+      (io.din === 0x34.asUInt()) -> 0x33.U(8.W),
+      (io.din === 0x35.asUInt()) -> 0x5b.U(8.W),
+      (io.din === 0x36.asUInt()) -> 0x5f.U(8.W),
+      (io.din === 0x37.asUInt()) -> 0x70.U(8.W),
+      (io.din === 0x38.asUInt()) -> 0x7f.U(8.W),
+      (io.din === 0x39.asUInt()) -> 0x7b.U(8.W)
     )
   )
   io.dout := dout
